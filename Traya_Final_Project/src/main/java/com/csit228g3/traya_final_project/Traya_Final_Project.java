f68ec3fbefb7dbdfa277bb67d0f06d2b3dbe542a
juniper06 <93751671+juniper06@users.noreply.github.com>
2023-04-28 23:01:36 +0800

    Add files via upload

---
+++ Traya_Final_Project/src/main/java/com/csit228g3/traya_final_project/Traya_Final_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.csit228g3.traya_final_project;
+
+/**
+ *
+ * @author Allan Traya
+ */
+public class Traya_Final_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
