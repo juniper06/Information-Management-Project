13a5675ac36298d881529a10e4ca35eae0ab5467
juniper06 <93751671+juniper06@users.noreply.github.com>
2023-04-28 10:52:05 +0800

    Add files via upload

---
+++ Gabriel_Final_Project/src/main/java/com/csit228g3/gabriel_final_project/Gabriel_Final_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.csit228g3.gabriel_final_project;
+
+/**
+ *
+ * @author Juniper Gabriel
+ */
+public class Gabriel_Final_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
