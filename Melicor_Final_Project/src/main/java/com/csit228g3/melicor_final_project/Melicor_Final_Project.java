1b8372af0fd7d1159f0fa486b0e0f4daef8787c6
juniper06 <93751671+juniper06@users.noreply.github.com>
2023-04-28 20:12:19 +0800

    Add files via upload

---
+++ Melicor_Final_Project/src/main/java/com/csit228g3/melicor_final_project/Melicor_Final_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.csit228g3.melicor_final_project;
+
+/**
+ *
+ * @author Juniper Gabriel
+ */
+public class Melicor_Final_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
